2d3edff3c1fad8071c2eedca1639c94b1dffefd9
Svitlana Rybakova <rsv0011@gmail.com>
2024-01-25 13:11:03 +0100

    implement the program interface

--- Program.cs
+++ Program.cs
@@ -10,7 +10,39 @@ namespace FlyRegistration
     {
         static void Main(string[] args)
         {
-            
+            int[] sectors = { 6, 28, 15, 15, 18 };
+            bool isOpen = true;
+
+            while (isOpen)
+            {
+                Console.SetCursorPosition(0, 18);
+                for (int i = 0; i < sectors.Length; i++)
+                {
+
+                    Console.BackgroundColor = ConsoleColor.Black;
+                    Console.ForegroundColor = ConsoleColor.DarkGreen;
+                    Console.WriteLine($"In sector {i + 1} available {sectors[i]} seats");
+                }
+
+                Console.SetCursorPosition(0, 0);
+                Console.BackgroundColor = ConsoleColor.Magenta;
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.WriteLine("The fly registration");
+
+                Console.BackgroundColor = ConsoleColor.Black;
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.WriteLine("\n\n1 - buy the seat(s) \n\n2 - exit the program");
+                Console.WriteLine("\n\nPlease, enter the number of command: ");
+
+                switch (Convert.ToInt32(Console.ReadLine()))
+                {
+                    case 1:
+                        break;
+                    case 2:
+                        isOpen = true;
+                        break;
+                }
+            }
         }
     }
 }
