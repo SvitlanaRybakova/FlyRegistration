28076b890d41150b27b2d494102b4a9654aa427d
Svitlana Rybakova <rsv0011@gmail.com>
2024-01-25 14:06:21 +0100

    Include the edge cases

--- Program.cs
+++ Program.cs
@@ -18,7 +18,6 @@ namespace FlyRegistration
                 Console.SetCursorPosition(0, 18);
                 for (int i = 0; i < sectors.Length; i++)
                 {
-
                     Console.BackgroundColor = ConsoleColor.Black;
                     Console.ForegroundColor = ConsoleColor.DarkBlue;
                     Console.WriteLine($"In sector {i + 1} available {sectors[i]} seats");
@@ -34,37 +33,44 @@ namespace FlyRegistration
                 Console.WriteLine("\n\n1 - buy the seat(s) \n\n2 - exit the program");
                 Console.Write("\n\nPlease, enter the number of command: ");
 
+                int userCommand;
+                if (!GetValidInput(Console.ReadLine(), out userCommand, 1, 2))
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Invalid command. Please enter a valid number (1 or 2).");
+                    Console.ReadKey();
+                    Console.Clear();
+                    continue;
+                }
 
-                switch (Convert.ToInt32(Console.ReadLine()))
+                switch (userCommand)
                 {
                     case 1:
-                        int userSector, userPlaceAmmount;
-                        Console.Write("Witch sector would you like to fly?: ");
-                        userSector = Convert.ToInt32(Console.ReadLine()) -1;
-
-                        if(sectors.Length <= userSector || userSector < 0)
+                        int userSector, userPlaceAmount;
+                        Console.Write("Which sector would you like to fly?: ");
+                        if (!GetValidInput(Console.ReadLine(), out userSector, 1, sectors.Length) || userSector <= 0)
                         {
                             Console.ForegroundColor = ConsoleColor.Red;
-                            Console.WriteLine("The specified sector is non-existent.");
+                            Console.WriteLine("Invalid sector. Please enter a valid sector number.");
                             break;
                         }
 
                         Console.ForegroundColor = ConsoleColor.White;
                         Console.Write("How many seats do you want to buy?: ");
-                        userPlaceAmmount = Convert.ToInt32(Console.ReadLine());
-
-                        if (sectors[userSector] < userPlaceAmmount || userPlaceAmmount < 0)
+                        if (!GetValidInput(Console.ReadLine(), out userPlaceAmount, 0, sectors[userSector - 1]) || userPlaceAmount <= 0)
                         {
                             Console.ForegroundColor = ConsoleColor.Red;
-                            Console.WriteLine($"The sector {userSector + 1} does not have enough seats. The remaining seats are {sectors[userSector]}.");
+                            Console.WriteLine($"Invalid input. Please enter a valid number of seats (1 to {sectors[userSector - 1]}).");
                             break;
                         }
-                        sectors[userSector] -= userPlaceAmmount;
+
+                        sectors[userSector - 1] -= userPlaceAmount;
                         Console.ForegroundColor = ConsoleColor.Green;
-                        Console.WriteLine($"Congrats, you have bought the {userPlaceAmmount} seats in the {userSector + 1} sector.");
+                        Console.WriteLine($"Congrats, you have bought {userPlaceAmount} seat(s) in sector {userSector}.");
                         break;
                     case 2:
-                        Console.WriteLine("Bye! Press the enter to exit the program");
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        Console.WriteLine("Bye! Press any key to exit the program");
                         isOpen = false;
                         break;
                 }
@@ -72,6 +78,11 @@ namespace FlyRegistration
                 Console.ReadKey();
                 Console.Clear();
             }
+        }
+
+        static bool GetValidInput(string userInput, out int result, int minValue, int maxValue)
+        {
+            return int.TryParse(userInput, out result) && result >= minValue && result <= maxValue;
         }
     }
 }
